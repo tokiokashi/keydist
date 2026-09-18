1a33c3b512e138a95a0856f499160ceee90bebf0
tokiokashi <Salvare116@gmail.com>
2026-09-18 17:48:08 +0900

    feat: 新下駄のsemanticを明示する (#224)

--- src/layouts/shingeta.ts
+++ src/layouts/shingeta.ts
@@ -11,9 +11,16 @@ import { faceFromEntries, fromFaces, type Face, type Layout } from './types.ts';
  * D面の重複する括弧や機能キーは採らず、単打面の `「` を残す。
  */
 
-const face = (trigger: string[], entries: Record<string, string>, layer?: string): Face => ({
+const face = (
+  trigger: string[],
+  entries: Record<string, string>,
+  layer?: string,
+  inputRole: Face['inputRole'] = 'layer',
+): Face => ({
   ...faceFromEntries(trigger, 'simultaneous', entries),
   layer,
+  inputRole,
+  ...(trigger.length > 0 ? { triggerBehavior: 'chord' as const } : {}),
 });
 
 export const SHINGETA_FACES: Face[] = [
@@ -27,7 +34,7 @@ export const SHINGETA_FACES: Face[] = [
     q: 'ふぁ', w: 'ご', e: 'ふ', r: 'ふぃ', t: 'ふぇ',
     a: 'ほ', s: 'じ', d: 'れ', f: 'も', g: 'ゆ',
     z: 'づ', x: 'ぞ', c: 'ぼ', v: 'む', b: 'ふぉ',
-  }, '中指シフト'),
+  }, '中指シフト', 'modifier'),
   face(['d'], {
     y: 'うぃ', u: 'ぱ', i: 'よ', o: 'み', p: 'うぇ', '[': 'うぉ',
     h: 'へ', j: 'あ', ';': 'え',
@@ -38,7 +45,7 @@ export const SHINGETA_FACES: Face[] = [
     q: 'ぢ', w: 'め', e: 'け', r: 'てぃ', t: 'でぃ',
     a: 'を', s: 'さ', d: 'お', f: 'り', g: 'ず',
     z: 'ぜ', x: 'ざ', c: 'ぎ', v: 'ろ', b: 'ぬ',
-  }, '薬指シフト'),
+  }, '薬指シフト', 'modifier'),
   face(['s'], {
     y: 'しぇ', u: 'ぺ', i: 'ど', o: 'や', p: 'じぇ',
     h: 'び', j: 'ら', ';': 'そ',
@@ -49,13 +56,13 @@ export const SHINGETA_FACES: Face[] = [
     q: 'ひゅ', w: 'しゅ', e: 'しょ', r: 'きゅ', t: 'ちゅ',
     a: 'ひょ', f: 'きょ', g: 'ちょ',
     z: 'ひゃ', c: 'しゃ', v: 'きゃ', b: 'ちゃ',
-  }),
+  }, undefined, 'composition'),
   face(['o'], {
     1: 'ょ', 2: 'ぴゃ', 3: 'ぴゅ', 4: 'ぴょ',
     q: 'りゅ', w: 'じゅ', e: 'じょ', r: 'ぎゅ', t: 'にゅ',
     a: 'りょ', f: 'ぎょ', g: 'にょ',
     z: 'りゃ', c: 'じゃ', v: 'ぎゃ', b: 'にゃ',
-  }),
+  }, undefined, 'composition'),
 ];
 
 const layout: Layout = fromFaces('shingeta', '新下駄', SHINGETA_FACES);
