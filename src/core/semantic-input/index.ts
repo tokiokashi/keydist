a4b953828d33b685da3534f95c8a95e6961d5e76
tokiokashi <Salvare116@gmail.com>
2026-09-19 21:38:08 +0900

    feat: export sequence source artifacts compiler

--- src/core/semantic-input/index.ts
+++ src/core/semantic-input/index.ts
@@ -1,4 +1,9 @@
-export { compileFaceSemanticInputs, compileSequenceSemanticInputs } from './compiler.ts';
+export {
+  compileFaceSemanticInputs,
+  compileSequenceInputArtifacts,
+  compileSequenceSemanticInputs,
+  type CompiledSequenceArtifacts,
+} from './compiler.ts';
 export type {
   FaceMembership,
   InputCapability,
