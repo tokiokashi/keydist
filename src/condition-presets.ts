f7765c46f3bc35b4b7475fd4e3dc6e96782bce23
tokiokashi <Salvare116@gmail.com>
2026-09-22 04:03:50 +0900

    refactor: route realization policy imports through core

--- src/condition-presets.ts
+++ src/condition-presets.ts
@@ -1,7 +1,9 @@
 import { sameChainPolicy } from './analysis-chain.ts';
 import { sameArpeggioPolicy } from './analysis-arpeggio.ts';
-import { sameTriggerRealizationPolicy } from './trigger-realization.ts';
-import { sameActionRealizationPolicy } from './core/semantic-input/action-realization.ts';
+import {
+  sameActionRealizationPolicy,
+  sameTriggerRealizationPolicy,
+} from './core/semantic-input/index.ts';
 import {
   DEFAULT_CONDITION_DEFAULTS,
   sanitizeConditionDefaults,
