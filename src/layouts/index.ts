7b518f2cb2914f94c4fa5df9c0776b924a359f80
tokiokashi <Salvare116@gmail.com>
2026-09-21 22:20:42 +0900

    refactor: stop exporting legacy Face classifiers

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -42,7 +42,7 @@ export {
   withRomaji,
   withCombos,
 } from './types.ts';
-export { canFoldFaces, classifyFaces, faceCells, groupFacesIntoLayers, handOfKey } from '../layers.ts';
+export { faceCells, handOfKey } from '../layers.ts';
 export type { FaceGroups, Hand, Layer } from '../layers.ts';
 
 const ROMAJI = kunrei();
