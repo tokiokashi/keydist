6f53f749fbd7d2a90076175819542395bf3636fc
tokiokashi <Salvare116@gmail.com>
2026-09-22 09:55:43 +0900

    refactor: clone logical trigger group selectors

--- src/condition-resolution.ts
+++ src/condition-resolution.ts
@@ -30,6 +30,9 @@ export function resolveConditions(
     },
     triggerActivationOverrides: (values.actionRealization.triggerActivationOverrides ?? []).map((item) => ({
       selector: {
+        ...(item.selector.triggerGroupId === undefined
+          ? {}
+          : { triggerGroupId: item.selector.triggerGroupId }),
         ...(item.selector.layerId === undefined ? {} : { layerId: item.selector.layerId }),
         ...(item.selector.triggerKeys === undefined ? {} : { triggerKeys: [...item.selector.triggerKeys] }),
       },
