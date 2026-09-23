559930908f2d303f949fcdafe26fc5946c8914e1
tokiokashi <Salvare116@gmail.com>
2026-09-23 10:22:21 +0900

    test(input): include lookup aggregation metadata

--- test/reverse-lookup.test.ts
+++ test/reverse-lookup.test.ts
@@ -60,5 +60,6 @@ test('reverseLookupStepLabelは1入力単位のaction順を表示する', () => 
     output: 'x',
     origin: 'face',
     actions: [['thumb-r'], ['h', 'j']],
+    aggregationGroupIds: ['layer:test'],
   }), '右親指 → H + J');
 });
