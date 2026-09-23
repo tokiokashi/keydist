139fe1bef8241b01bd78b8e7473aae261381346b
tokiokashi <Salvare116@gmail.com>
2026-09-23 10:30:01 +0900

    fix(test): include lookup aggregation metadata

--- test/reverse-lookup.test.ts
+++ test/reverse-lookup.test.ts
@@ -50,6 +50,7 @@ test('reverseLookupRouteLabelはchordとsequenceを区別して表示する', ()
       output: 'x',
       origin: 'face',
       actions: [['thumb-r'], ['h', 'j']],
+      aggregationGroupIds: ['layer:test'],
     }],
   }), '右親指 → H + J');
 });
