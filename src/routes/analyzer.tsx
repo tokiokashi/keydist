7b1d5e76e55c24bc66630c9bdf0471a27a4c05e8
tokiokashi <Salvare116@gmail.com>
2026-09-26 18:25:55 +0900

    refactor: legacy参照とsubpath importを更新する
    Refs #544

--- src/routes/analyzer.tsx
+++ src/routes/analyzer.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { AnalyzerPage } from '../analyzer-page.tsx';
+import { AnalyzerPage } from '#legacy/analyzer-page.tsx';
 import analyzerCss from '../style.css?url';
 
 export const Route = createFileRoute('/analyzer')({
