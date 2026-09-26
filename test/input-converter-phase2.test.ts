9f06dd78746be824bd2519775dee659cc2d7d10f
tokiokashi <Salvare116@gmail.com>
2026-09-26 23:26:51 +0900

    refactor: test importを新しい構造へ追従する
    Refs #544

--- test/input-converter-phase2.test.ts
+++ test/input-converter-phase2.test.ts
@@ -1,7 +1,7 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
-import { TypingInputEngine } from '../src/core/input-converter/index.ts';
-import { browserKeyboardEventToPhysicalKeyEvent } from '../src/features/input-converter/browser-keyboard-adapter.ts';
+import { TypingInputEngine } from '#tester/engine/index.ts';
+import { browserKeyboardEventToPhysicalKeyEvent } from '#tester/browser-keyboard-adapter.ts';
 import { NAGINATA_V18 } from '#input/layouts/naginata.ts';
 import { SHINGETA } from '#input/layouts/shingeta.ts';
 import { SHIN_JIS_SIMULTANEOUS } from '#input/layouts/shin-jis.ts';
